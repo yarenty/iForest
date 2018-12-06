aa009c616f333a4f06e9ac5d00d3fc82461d250a
yarenty <yarenty@gmail.com>
2018-12-06 11:23:55 +0000

    cleaning

--- src/main/scala/com/yarenty/h2o/DoAnomaly.scala
+++ src/main/scala/com/yarenty/h2o/DoAnomaly.scala
@@ -13,19 +13,14 @@ object DoAnomaly extends App {
   H2OStarter.start(Array("-name", "YOLO", "--ga_opt_out"), System.getProperty("user.dir"))
 
 
-  println("ready to GO")
-
-  Log.info("Ready to GO!")
-
   Log.info("load data")
-  
- val train = new H2OFrame(new URI("/opt/data/demo/EH_BL1375B/data2.csv"))
-  
+
+  val train = new H2OFrame(new URI("/opt/data/demo/EH_BL1375B/data2.csv"))
+
   val params = new IsolationForestParameters()
-    params._train = train.key
-
-
+  params._train = train.key
   params._mtries = -1
+  
   params._sample_size = 12
   params._max_depth = 3 // log2(_sample_size) = 8
 
@@ -34,18 +29,19 @@ object DoAnomaly extends App {
   params._min_split_improvement = 0
   params._nbins = 2
   params._nbins_cats = 2
-  
+
   params._ntrees = 100
-  
+
   val iForest = new IsolationForest(params)
 
   val iModel = iForest.trainModel.get
+
+  
   
   val predicitons = iModel.score(train)
-  
+
   val summary = train.add(predicitons)
-
-
-//  println(iModel.getMostImportantFeatures(10).mkString(";\n"))
   
+  //  println(iModel.getMostImportantFeatures(10).mkString(";\n"))
+
 }
